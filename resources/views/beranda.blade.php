34efd295a743d19f405c7cb1253e6132bf97d8c9
imaliarosyidaa <imaliarosyidaa@gmail.com>
2024-06-01 09:12:56 +0700

    add custom fonts & color,icon, template halaman, navbar baru

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<div>
+    <p>Buat halaman seperti ini di /pages</p>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,24 @@
 <!doctype html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('js/app.js') }}" rel="stylesheet">
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  {{--Navbar--}}
+  @include('partials.navbar')
+
+  {{--Main--}}
+  <div>
+    @yield('content')
+  </div>
+
+  {{--Foter--}}
+  @include('partials.footer')
+  
 </body>
 </html>
