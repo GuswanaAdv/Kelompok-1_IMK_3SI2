7b0334506f6fb8f7531712882375cf4368c736d6
nsdan <nurshifadani23@gmail.com>
2024-06-15 00:49:53 +0700

    Menghubungkan Berita dan Halaman Berita

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -19,6 +19,47 @@ class BeritaController extends Controller
         ]);
     }
 
+    public function beritaPage($slug)
+    {
+        try {
+            // Menggunakan where untuk mencari berdasarkan slug
+            $berita = Berita::where('slug', $slug)->first();
+    
+            if (!$berita) {
+                return response()->json(['error' => 'Berita tidak ditemukan'], 404);
+            }
+    
+            // Gantikan [h] dengan <h1> dan [p] dengan <p>
+            $berita->konten = str_replace('[h]', '<h3 class="text-xl font-bold text-green-700 mb-0">', $berita->konten);
+            $berita->konten = str_replace('[/h]', '</h3>', $berita->konten);
+            $berita->konten = str_replace('[p]', '<p class="text-lg">', $berita->konten);
+            $berita->konten = str_replace('[/p]', '</p>', $berita->konten);
+    
+            // Tambahkan spasi antara paragraf dan judul
+            $berita->konten = str_replace("\n", '<br>', $berita->konten);
+        
+             // Get 4 articles with the same category, sorted by newest first
+            $relatedArticles = Berita::where('id', '!=', $berita->id) // Exclude the current article itself
+                ->where(function ($query) use ($berita) {
+                    $categories = explode(',', $berita->kategori);
+                    foreach ($categories as $category) {
+                        $query->orWhere('kategori', 'like', '%' . trim($category) . '%');
+                    }
+                })
+                ->orderBy('created_at', 'desc') // Sort by newest first
+                ->limit(4)
+                ->get();
+
+            return view('berita_page', [
+                'judul_halaman' => "Halaman Berita",
+                'berita' => $berita,
+                'relatedArticles' => $relatedArticles
+            ]);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
     public function search(Request $request)
     {   
         $search = $request->input('cari_berita1');
