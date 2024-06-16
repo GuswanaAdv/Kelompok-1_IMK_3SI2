2e3a94f74fa9b89d614800327c5f01034ac20ced
nsdan <nurshifadani23@gmail.com>
2024-06-17 01:08:03 +0700

    Fix footer mobile responsive

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="bg-gradient-to-r from-lime-500 from-5% via-green via-10% to-darkgreen to-95% text-white py-4 mt-4">
     <div class="container mx-auto px-4">
         <div class="flex flex-col justify-evenly sm:flex-row md:items-start border-b border-gray-300 pb-4">
-            <div class="flex space-x-2 py-4 px-16">
+            <div class="flex space-x-2 py-4 px-16 justify-center sm:justify-start">
                 <img src="https://jatinegara.kec-sempor.kebumenkab.go.id/assets/logo/kbm.png" alt="Logo" class="h-10">
                 <h2 class="text-lg font-bold">Desa Jatinegara</h2>
             </div>
@@ -12,7 +12,7 @@
                     <img src="https://www.aandrijftechnischburo.nl/files/theme/images/waicon.png" alt="WhatsApp" class="h-6">
                     <span>WhatsApp</span>
                 </a>
-                <a href="https://www.facebook.com/adm.pemdes.7" class="flex md:items-center space-x-2 mb-2 md:mb-0">
+                <a href="https://www.facebook.com/adm.pemdes.7" class="flex pr-1 sm:pr-0 justify-end sm:justify-start md:items-center space-x-2 mb-2 md:mb-0">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Facebook_icon_2013.svg/300px-Facebook_icon_2013.svg.png" alt="Facebook" class="h-6">
                     <span>Facebook</span>
                 </a>
@@ -20,7 +20,7 @@
                     <img src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="Twitter" class="h-6">
                     <span>Twitter</span>
                 </a>
-                <a href="https://www.instagram.com/humas.jatinegara" class="flex md:items-center space-x-2 mb-2 md:mb-0">
+                <a href="https://www.instagram.com/humas.jatinegara" class="flex justify-end sm:justify-start md:items-center space-x-2 mb-2 md:mb-0">
                     <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png" alt="Instagram" class="h-6">
                     <span>Instagram</span>
                 </a>
