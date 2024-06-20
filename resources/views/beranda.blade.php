67c4f6e891cabea7378b962f4986d4401a08b2a3
Guswana Adv <guswanaadventus12345@gmail.com>
2024-06-20 20:34:17 +0700

    memperbaiki gambar di halaman pemerintah dan fasilitas kesehatan

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -8,10 +8,14 @@
             <div class="w-full flex justify-center lg:justify-start h-24">
                 <div class="pt-8 lg:pt-4 grid grid-cols-2 w-4/5 lg:w-max gap-1 lg:gap-1">
                     <div>
-                        <button class="bg-darkgreen rounded-[36px] text-white px-8 lg:px-12 py-1 lg:py-3 lg:mr-4 font-inter outline outline-darkgreen">Cari Data</button>
+                        <a href="{{route('datadesa')}}">
+                            <button class="bg-darkgreen rounded-[36px] text-white px-8 lg:px-12 py-1 lg:py-3 lg:mr-4 font-inter outline outline-darkgreen">Cari Data</button>
+                        </a>
                     </div>
                     <div>
-                        <button class="border-1 border-darkgreen rounded-[36px] px-4 lg:px-8 py-1 lg:py-3 outline outline-darkgreen font-inter">Tentang Desa</button>
+                        <a href="{{route('profil')}}">
+                            <button class="border-1 border-darkgreen rounded-[36px] px-4 lg:px-8 py-1 lg:py-3 outline outline-darkgreen font-inter">Tentang Desa</button>
+                        </a>
                     </div>
                 </div>
             </div>
--- resources/views/menu_profil/profil_pemerintahan.blade.php
+++ resources/views/menu_profil/profil_pemerintahan.blade.php
@@ -17,7 +17,7 @@
             <div class="mx-auto max-w-2xl px-4 py-4">
                 <h2 class="text-center mb-4">Kepala Desa</h2>
                 <div class="flex flex-col items-center gap-y-1">
-                    <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                    <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Bupati_Kebumen_Arif_Sugiyanto.jpg/220px-Bupati_Kebumen_Arif_Sugiyanto.jpg" width="100"></div>
                     <div>
                         <p class="text-center">Kepala Desa</p>
                         <p class="text-center">Sunaryo</p>
@@ -28,7 +28,7 @@
             <div class="mx-auto max-w-2xl border-t px-4 py-4">
                 <h2 class="text-center mb-4">Sekretaris Desa</h2>
                 <div class="flex flex-col items-center gap-y-1">
-                    <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                    <div><img src="https://upload.wikimedia.org/wikipedia/id/b/b2/Yahya.jpg" width="100"></div>
                     <div>
                         <p class="text-center">Sekretaris Desa</p>
                         <p class="text-center">Sugianto, SE</p>
@@ -40,21 +40,21 @@
                 <h2 class="text-center mb-4">Kepala Seksi Desa</h2>
                 <div class="grid grid-cols-3 gap-x-6 gap-y-10">
                     <div class="flex flex-col items-center gap-y-1">
-                    <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                    <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Wakil_Bupati_Kebumen_Ristawati_Purwaningsih.jpg/220px-Wakil_Bupati_Kebumen_Ristawati_Purwaningsih.jpg" width="100px"></div>
                         <div>
                             <p class="text-center">Kepala Seksi Pemerintahan</p>
                             <p class="text-center">Ike Krisbianti, S.Sos</p>
                         </div>
                     </div>
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://old.cirebonkab.go.id/wp-content/uploads/2020/06/Bupati-Cirebon-2019-OK-1-683x1024.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Seksi Kesejahteraan</p>
                             <p class="text-center">Purwanto</p>
                         </div>
                     </div>
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Wahyu_Mijaya_Pj_Bupati_Cirebon.jpg/200px-Wahyu_Mijaya_Pj_Bupati_Cirebon.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Seksi Pelayanan</p>
                             <p class="text-center">Kusdi Ferlina</p>
@@ -67,21 +67,21 @@
                 <h2 class="text-center mb-4">Kepala Urusan Desa</h2>
                 <div class="grid grid-cols-3 gap-x-6 gap-y-10">
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/Budhi_Sarwono.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Urusan Keuangan</p>
                             <p class="text-center">Marsudi</p>
                         </div>
                     </div>
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://upload.wikimedia.org/wikipedia/id/7/7d/Wahyu_Tjiptaningsih_Wabup_Cirebon.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Urusan Tata Usaha dan Umum</p>
                             <p class="text-center">Yuni Iriyanti, S.Pd</p>
                         </div>
                     </div>
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxEyt0qORg3mKsSh8gPQacBRiXHD2-10C67A&s" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Urusan Perencanaan</p>
                             <p class="text-center">Muhajir Wardani, SE</p>
@@ -94,21 +94,21 @@
                 <h2 class="text-center mb-4">Kepala Dusun</h2>
                 <div class="grid grid-cols-3 gap-x-6 gap-y-10">
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://disipusda.purwakartakab.go.id/wp-content/uploads/2023/09/WakilBupati-02-Drs.-H.-DUDUNG-B-SUPARDI-MM.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Dusun 1</p>
                             <p class="text-center">Suswadi</p>
                         </div>
                     </div>
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://disipusda.purwakartakab.go.id/wp-content/uploads/2023/09/Bupati-Purwakarta-08-H.-Dedi-Mulyadi-SH.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Dusun 2</p>
                             <p class="text-center">Purwito</p>
                         </div>
                     </div>
                     <div class="flex flex-col items-center gap-y-1">
-                        <div><img src="http://localhost:8000/img/foto_profil.jpg" width="180"></div>
+                        <div><img src="https://disipusda.purwakartakab.go.id/wp-content/uploads/2023/09/Wakil-Bupati-04-H.-Aming-1024x1024.jpg" width="100"></div>
                         <div>
                             <p class="text-center">Kepala Dusun 3</p>
                             <p class="text-center">Wahidin</p>
